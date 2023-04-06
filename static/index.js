5e1d6a7d3659db3d7da53c1ad8adeac91ee22fb4
Himanshu Kabra <himanshukabra2212@gmail.com>
2023-04-07 02:03:05 +0530

    connected frontend and backend

---
+++ static/index.js
@@ -0,0 +1,15 @@
+const submit = document.querySelector('#submit')
+const value = document.querySelector('#textbox')
+
+submit.addEventListener('click',async()=>{
+    let data = await fetch('/',{
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ url: `${value.value}`})
+    })
+    data = await data.json()
+    if(data.status === "ok")
+    value.value = `http://short-jjwl.onrender.com/s/${data.url}`
+    else
+    value.value = `${data.message}`
+})
